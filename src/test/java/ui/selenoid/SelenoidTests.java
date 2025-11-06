6b4149d61905bb56cd7f007f196dbf1974f9c941
Svetlana.Terekhova <Svetlana.Terekhova@PRD-BPMS-002>
2025-11-06 22:06:34 +0300

    selenium через selenoid UI

--- src/test/java/ui/selenoid/SelenoidTests.java
+++ src/test/java/ui/selenoid/SelenoidTests.java
@@ -2,10 +2,13 @@ package ui.selenoid;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelenoidTests {
  /*   @Test
@@ -18,17 +21,42 @@ public class SelenoidTests {
 
     @Test
     public void selenoidDockerTest() throws Exception {
-        // Настраиваем capabilities (браузер, версия, опции и т.п.)
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        //      capabilities.setVersion("latest"); // или конкретную, например "120.0"
-        capabilities.setVersion("128");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        // Настраиваем ChromeOptions (W3C синтаксис)
+        ChromeOptions options = new ChromeOptions();
+        options.setBrowserVersion("128");
+        
+        // Настраиваем опции Selenoid через selenoid:options (W3C синтаксис)
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", true);
+        options.setCapability("selenoid:options", selenoidOptions);
 
         // Подключаемся к Selenoid
         URL selenoidUrl = new URL("http://localhost:4444/wd/hub");
-        WebDriver driver = new RemoteWebDriver(selenoidUrl, capabilities);
+        WebDriver driver = new RemoteWebDriver(selenoidUrl, options);
+
+        // Тест
+        driver.get("https://vk.com");
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+
+    @Test
+    public void selenoidFirefoxTest() throws Exception {
+        // Настраиваем FirefoxOptions (W3C синтаксис)
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBrowserVersion("latest");
+        
+        // Настраиваем опции Selenoid через selenoid:options (W3C синтаксис)
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", true);
+        options.setCapability("selenoid:options", selenoidOptions);
+
+        // Подключаемся к Selenoid
+        URL selenoidUrl = new URL("http://localhost:4444/wd/hub");
+        WebDriver driver = new RemoteWebDriver(selenoidUrl, options);
 
         // Тест
         driver.get("https://vk.com");
